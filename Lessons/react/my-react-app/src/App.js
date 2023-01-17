1b8051a5397508f14fff54ac22a11ea0c3732fe9
rikords <79376651+rikords@users.noreply.github.com>
2023-01-17 23:49:54 +0300

    update

--- Lessons/react/my-react-app/src/App.js
+++ Lessons/react/my-react-app/src/App.js
@@ -3,22 +3,29 @@ import "./App.css";
 import Card from "./components/Card";
 
 let arr = [
-  ["Dağ1", "Açıklama 1"],
-  ["Dağ2", "Açıklama 2"],
-  ["Dağ3", "Açıklama 3"],
+  {
+    title: "Dağ 1",
+    par: "Açıklama 1",
+  },
+  {
+    title: "Dağ 2",
+    par: "Açıklama 2",
+  },
+  {
+    title: "Dağ 3",
+    par: "Açıklama 3",
+  },
 ];
-function App() {
-  return (
-    <div>
-      <h1>Başlık</h1>
-      <p>Başlangıç</p>
-      <div className="Cards">
-        {arr.map(function ([title, par]) {
-          return <Card title={title} par={par} />;
-        })}
-      </div>
+const App = () => (
+  <div>
+    <h1>Başlık</h1>
+    <p>Başlangıç</p>
+    <div className="Cards">
+      {arr.map(({ title, par }) => (
+        <Card par={par} title={title} />
+      ))}
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
--- Lessons/react/my-react-app/src/components/Card/index.js
+++ Lessons/react/my-react-app/src/components/Card/index.js
@@ -1,12 +1,11 @@
 import "./Card.css";
-function Card({ title, par }) {
-  return (
-    <div className="Card">
-      <h6>{title}</h6>
-      <p>{par}</p>
-      <button>Devamı</button>
-    </div>
-  );
-}
+
+const Card = ({ title, par }) => (
+  <div className="Card">
+    <h6>{title}</h6>
+    <p>{par}</p>
+    <button>Devamı</button>
+  </div>
+);
 
 export default Card;
