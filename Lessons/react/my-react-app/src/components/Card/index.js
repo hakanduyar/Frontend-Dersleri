b5ba65ddb999e8fe39ac6745cc80f55f02d1a24a
rikords <79376651+rikords@users.noreply.github.com>
2023-01-27 23:04:04 +0300

    Update index.js

--- Lessons/react/my-react-app/src/components/Card/index.js
+++ Lessons/react/my-react-app/src/components/Card/index.js
@@ -1,15 +1,31 @@
-import "./Card.css";
+import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
 
-const Card = ({ title, par, lesson, index }) => (
-  <div className="Card">
-    <h6>{title}</h6>
-    <b>{index}</b>
-    <p>{par}</p>
-    {/* <p>{lesson}</p> */}
-    {/* {lesson > 20 ? <div>İleri düzey</div> : <div>Başlangı</div>} */}
-    {lesson > 20 && index == 0 && <div>İleri düzey</div>}
-    <button>Devamı</button>
-  </div>
+const CardComponent = ({ title, par, lesson, index }) => (
+  <Card shadow="sm" p="lg" radius="md" withBorder>
+    <Card.Section>
+      <Image
+        src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+        height={160}
+        alt="Norway"
+      />
+    </Card.Section>
+
+    <Group position="apart" mt="md" mb="xs">
+      <Text weight={500}>Norway Fjord Adventures</Text>
+      <Badge color="pink" variant="light">
+        On Sale
+      </Badge>
+    </Group>
+
+    <Text size="sm" color="dimmed">
+      With Fjord Tours you can explore more of the magical fjord landscapes with
+      tours and activities on and around the fjords of Norway
+    </Text>
+
+    <Button variant="light" color="blue" fullWidth mt="md" radius="md">
+      Book classic tour now
+    </Button>
+  </Card>
 );
 
-export default Card;
+export default CardComponent;
